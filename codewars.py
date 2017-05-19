febf5c663e78a3ed5e5e855df8a23d8078dd672c
James Salamonsen <Woojgh@Jts-MacBook-Pro.local>
2017-05-19 08:17:48 -0700

    more katas

--- codewars.py
+++ codewars.py
@@ -58,5 +58,15 @@ def comp(array1, array2):
 '''ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
    If the function is passed a valid PIN string, return true, else return false.'''
 def validate_pin(pin):
-	
+	if len(str(pin)) == 4 or len(str(pin)) == 6:
+		return True
+	else:
+		return False
+# Test.assert_equals(validate_pin("1"),False, "Wrong output for '1'")
+# Test.assert_equals(validate_pin("12"),False, "Wrong output for '12'")
+''''''
+
+
+
+
 	
