3e9f1f8e27666310afeb35a66294d87a129aac13
James Salamonsen <Woojgh@Jts-MacBook-Pro.local>
2017-05-19 04:00:22 -0700

    adding more katas

--- codewars.py
+++ codewars.py
@@ -1,3 +1,4 @@
+import math
 from math import sqrt
 
 '''Implement a method that accepts 3 integer values a, b, c. 
@@ -5,9 +6,21 @@ from math import sqrt
    with the sides of given length and false in any other case.'''
 
 def is_triangle(a, b, c):
-    sur = (a + b + c) / 2
-    area = (sur * (sur - a) * (sur - b) * (sur- c))**0.5
-    if area > 0:
-        return True
-    else:
-        return False
+	inputs = sorted([a, b, c])
+	if (inputs[0] + inputs[1]) <= inputs[2]:
+		return True
+	else:
+		return False
+
+'''In this kata, you are asked to square every digit of a number.
+   If we run 9119 through the function, 811181 will come out.'''
+
+def square_digits(num):
+	# ret = ""
+    # for x in str(num):
+    #     ret += str(int(x)**2)
+    # return int(ret)
+	joined = lambda squared_list: int(''.join(str(i) for i in squared_list))
+	return joined([int(i)**2 for i in str(num)])
+
+''' '''
